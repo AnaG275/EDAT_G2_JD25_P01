3e8a6783fc7c6955e4050e0c177544b87c7db676
AnaG275 <24030358@itesa.edu.mx>
2025-08-29 12:57:34 -0600

    Add files via upload

---
+++ Datos.Aleatorios/frmDatosAleatorios.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Datos.Aleatorios
+{
+    partial class frmDatosAleatorios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(33, 22);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(708, 321);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // frmDatosAleatorios
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "frmDatosAleatorios";
+            Text = "frmDatosAleatorios";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+    }
+}
